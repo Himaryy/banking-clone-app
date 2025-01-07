339951ad213d0770373fde96f167dcdf8bd9db06
Himaryy <122810667+Himaryy@users.noreply.github.com>
2025-01-07 18:25:42 +0700

    implement login/register UI

---
+++ lib/action/user.action.ts
@@ -0,0 +1,17 @@
+"use server";
+
+export const signIn = async () => {
+  try {
+    // Mutation / database / make fetch
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+export const signUp = async () => {
+  try {
+    // Mutation / database / make fetch
+  } catch (error) {
+    console.log(error);
+  }
+};
